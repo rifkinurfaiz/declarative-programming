772dd0c23b38688f0ab281b76e6cb775152a4145
Rifki Nurfaiz <rifki.nurfaiz@bankneo.co.id>
2022-06-06 23:26:54 +0700

    Implement insert and isExists tree function

--- src/4-tree.fsx
+++ src/4-tree.fsx
@@ -11,3 +11,15 @@ let rec countNode = function
 let rec treeDepth = function
     | Leaf -> 0
     | Node(v, branch1, branch2) -> 1 + max (treeDepth branch1) (treeDepth branch2);;
+
+let rec isExists = function
+    | (Leaf, e) -> false
+    | (Node(v, branch1, branch2), e) -> if (e < v) then isExists(branch1, e) 
+                                                                      elif (e > v) then isExists(branch2, e)
+                                                                      else true;;
+
+let rec insert = function
+    | (Leaf, e) -> Node(e, Leaf, Leaf)
+    | (Node(v, branch1, branch2), e) -> if (e < v) then Node(v, insert(branch1, e), branch2)
+                                        elif (e > v) then Node(v, branch1, insert(branch2, e))
+                                        else Node(v, branch1, branch2);;
